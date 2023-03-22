3f2c7541bc8c4271479cff1a7d68dd4b5f5bda61
Leonid <leom@list.ru>
2023-03-22 14:43:24 +0300

    KS-1-2 печать в консоль lesson_1

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val textGratitude: String = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(textGratitude)
+
+    var numberEmployees: Int = 2000
+//    println(numberEmployees)
+
+    numberEmployees = 1999
+    println(numberEmployees)
+
+}
