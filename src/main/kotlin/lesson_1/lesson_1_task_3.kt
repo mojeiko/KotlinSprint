68f9bc38ed26a3a0946f3421ae2afa6d92066ce8
Leonid <leom@list.ru>
2023-03-23 11:23:03 +0300

    KS-1-3 переменные var и val

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -2,6 +2,17 @@ package lesson_1
 
 fun main() {
 
-//
+    val year: Int = 1961      //val - нет необходимости менять значение
+    var hour: String = "09"   //var - есть необходимость изменить значение
+    var minute: String = "07" //var - есть необходимость изменить значение
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    println ("$hour:$minute")
 
 }
