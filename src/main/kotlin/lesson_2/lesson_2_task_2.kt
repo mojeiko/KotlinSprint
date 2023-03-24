5f82b8410136139acd1d60779854402f652e9ef5
Leonid <leom@list.ru>
2023-03-24 18:02:10 +0300

    KS-2-2 расчет зарплаты

---
+++ src/main/kotlin/lesson_2/lesson_2_task_2.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main() {
+
+    val salaryOfEmployees: Int = 50 * 30000
+    val totalSalary: Int = salaryOfEmployees + 30 * 20000
+    val averageSalary: Int = totalSalary / (50 + 30)
+
+    println(salaryOfEmployees)
+    println(totalSalary)
+    println(averageSalary)
+
+}
