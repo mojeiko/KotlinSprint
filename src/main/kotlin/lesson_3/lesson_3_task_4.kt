e116738ce5bbe6a98e3040b581ef6e25efe347a1
mojeiko <leom@list.ru>
2023-03-26 12:45:03 +0300

    KS-3-4 шахматный ход

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -2,20 +2,6 @@ package lesson_3
 
 fun main() {
 
-    var numMove: Int = 1
-    var moveFrom: String = "E2"
-    var moveTo: String = "E4"
 
-    var strSendMove: String = "$moveFrom-$moveTo;$numMove"
-
-    println(strSendMove)
-
-    numMove++
-    moveFrom = "D2"
-    moveTo = "D3"
-
-    strSendMove = "$moveFrom-$moveTo;$numMove"
-
-    println(strSendMove)
 
 }
