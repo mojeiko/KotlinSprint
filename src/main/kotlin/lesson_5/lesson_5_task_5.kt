9482afeee48f98e71586bea3e9ead684bbcd3f00
mojeiko <leom@list.ru>
2023-04-02 00:28:27 +0300

    KS-5-5 лотерея улучшенная

--- src/main/kotlin/lesson_5/lesson_5_task_5.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_5.kt
@@ -1,11 +1,9 @@
 package lesson_5
 
-import java.util.*
-
 fun main() {
 
-    val number1: Int = Random().nextInt(100) + 1
-    val number2: Int = Random().nextInt(100) + 1
+    val number1: Int = (1..5).random()
+    val number2: Int = (1..5).random()
 
     println("Лотерея \"Угадай два числа!\"\n")
 
@@ -15,18 +13,17 @@ fun main() {
     print("Введите второе число (от 1 до 100): ")
     val inputNumber2 = readln().toInt()
 
-    val compare1 = (number1 == inputNumber1 || number1 == inputNumber2) &&
-            (number2 == inputNumber1 || number2 == inputNumber2)
-    val compare2 = (number1 == inputNumber1 || number1 == inputNumber2) ||
-            (number2 == inputNumber1 || number2 == inputNumber2)
+    val compare1 =
+        (number1 == inputNumber1 || number1 == inputNumber2) && (number2 == inputNumber1 || number2 == inputNumber2)
+    val compare2 =
+        (number1 == inputNumber1 || number1 == inputNumber2) || (number2 == inputNumber1 || number2 == inputNumber2)
 
     val result = when {
-        compare1 -> "Поздравляем! Вы выиграли главный приз!"
-        compare2 -> "Вы выиграли утешительный приз!"
-        else -> "Неудача! Крутите барабан!"
+        compare1 -> "Поздравляем! Вы выиграли главный приз!\n"
+        compare2 -> "Вы выиграли утешительный приз!\n"
+        else -> "Неудача! Крутите барабан!\n"
     }
 
-    println()
     println(result)
     println("Правильные числа: $number1 и $number2")
 
